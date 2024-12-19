d354143184928c997d70960682b9dd6027d697f9
ponnanna <ponnanna.ks@gmail.com>
2024-12-19 23:36:23 +0530

    Initial Commit

---
+++ PCrypto/PCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCryptoApp.swift
+//  PCrypto
+//
+//  Created by Logycent on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
